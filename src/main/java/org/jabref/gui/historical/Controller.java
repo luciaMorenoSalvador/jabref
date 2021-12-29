9662f03d041d7d5601817b84f8ddc7f6394685c3
luciaMorenoSalvador <l.salvador@campus.fct.unl.pt>
2021-12-29 01:48:29 +0100

    historical

--- src/main/java/org/jabref/gui/historical/Controller.java
+++ src/main/java/org/jabref/gui/historical/Controller.java
@@ -1,6 +1,7 @@
 package org.jabref.gui.historical;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javafx.fxml.FXML;
@@ -8,18 +9,19 @@ import javafx.scene.control.DialogPane;
 
 import org.jabref.model.entry.BibEntry;
 
+@SuppressWarnings("checkstyle:NoWhitespaceBefore")
 public class Controller {
 
     @FXML
     private DialogPane pane;
 
-    private List<BibEntry> entries ;
+    private List<Collection<String>> entries ;
 
     public void initialize() {
-        entries = new ArrayList<>();
+        entries = new ArrayList<java.util.Collection<String>>();
         for (int i = 1; i <= 1000; i++) {
             BibEntry entry = new BibEntry();
-            entries.add(entry);
+            entries.add(entry.getFieldValues());
             pane.getChildren().add(entry);
         }
     }
